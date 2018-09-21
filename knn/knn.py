ad7443c4cd29a7afeaa358f2390fd21d83e5a0d9
yuxia <1464567365@qq.com>
2018-09-21 09:30:37 +0800

    Add knn code

---
+++ knn/knn.py
@@ -0,0 +1,23 @@
+from numpy import *
+from collections import defaultdict
+
+
+def classify(in_x, date_set, labels, k):
+    data_size = date_set.shape[0]
+    data = tile(in_x, (data_size, 1))
+    data_diff = data - date_set
+    distance = data_diff ** 2
+    distance = distance.sum(axis=1) ** 0.5
+    sort_index = distance.argsort()
+    label_count_map = defaultdict(int)
+    for i in range(0, k):
+        label_count_map[labels[sort_index[i]]] = label_count_map[labels[sort_index[i]]] + 1
+    sorted_class_count = sorted(label_count_map.items(), key=lambda item: item[1], reverse=True)
+    return sorted_class_count[0][0]
+
+
+if __name__ == '__main__':
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    label_arr = ['A', 'A', 'B', 'B']
+    test_x = [0, 0]
+    print(classify(test_x, group, label_arr, 1))
